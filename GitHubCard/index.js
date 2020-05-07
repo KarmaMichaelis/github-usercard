7289f82d3708e403e52bbcc3accbec94cfb8c69e
Karma Michealis <achantayd@gmail.com>
2020-05-07 18:41:12 -0400

    update files

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,20 @@
     https://api.github.com/users/<your name>
 */
 
+
+  axios.get('https://api.github.com/users/KarmaMichaelis')
+.then(response=>{
+  console.log(response)
+})
+.catch(error=>{
+  console.log('error')
+})
+.finally(()=>{
+  console.log('complete')
+})
+
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
